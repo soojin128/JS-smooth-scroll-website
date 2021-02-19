5ab52d260a8ac3d58dc0000ab6bb65c4361d0df4
Jenny Kim <kimsujin128@naver.com>
2021-02-19 16:15:20 +0900

    modify hide mobile menu width

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ window.addEventListener('click', highlightMenu);
 // Close mobile menu when clicking on a menu item
 const hideMobileMenu = () => {
   const menuBars = document.querySelector('.is-active');
-  if(window.innerWidth <= 768 && menuBars){
+  if(window.innerWidth <= 960 && menuBars){
     menu.classList.toggle('is-active');
     menuLinks.classList.remove('active');
   }
