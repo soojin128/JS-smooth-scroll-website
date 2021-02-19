7a63446ed3760d9b682e6542aa2f558120c7f6b0
Jenny Kim <kimsujin128@naver.com>
2021-02-19 16:29:59 +0900

    check github

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const highlightMenu = () => {
   const aboutMenu = document.querySelector('#about-page');
   const servicesMenu = document.querySelector('#services-page');
   let scrollPos = window.scrollY;
-
+console.log(scrollPos)
   // adds 'highlight' class to my menu items
   if(window.innerWidth > 960 && scrollPos < 600){
     homeMenu.classList.add('highlight');
@@ -47,7 +47,7 @@ window.addEventListener('click', highlightMenu);
 // Close mobile menu when clicking on a menu item
 const hideMobileMenu = () => {
   const menuBars = document.querySelector('.is-active');
-  if(window.innerWidth <= 960 && menuBars){
+  if(window.innerWidth <= 768 && menuBars){
     menu.classList.toggle('is-active');
     menuLinks.classList.remove('active');
   }
